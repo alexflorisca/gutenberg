5ec9ac3f53b0e5c5cbf15e1919655c0ffac9d73d
Jorge Costa <jorge.costa@developer.pt>
2024-09-27 18:57:38 +0100

    no styles drill down; disable style books click;

--- packages/edit-site/src/components/layout/index.js
+++ packages/edit-site/src/components/layout/index.js
@@ -213,6 +213,7 @@ export default function Layout( { route } ) {
 													gradientValue ??
 													backgroundColor,
 											} }
+											hasResize={ widths?.hasResize }
 										>
 											{ areas.preview }
 										</ResizableFrame>
